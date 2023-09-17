1dd8b024ac1d6776b4a66ab0ce3be0dab74f038d
Sawairazia <sawairazia3@gmail.com>
2023-09-17 22:13:21 +0500

    ok

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography;
+
+namespace Assignment_1
+{
+    partial class string_programs
+    {
+        static void Main(String[] args)
+        {
+            File1();
+            File2();
+        }
+
+        static void File1()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 50;
+            bool DrivingTestPassed = true;
+            float PassingRate = 3.99999f;
+        }
+
+        static void File2()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 49;
+            bool DrivingTestPassed = true;
+            float PassingRate = 2.99999f;
+        }
+    }
+}
+
+
