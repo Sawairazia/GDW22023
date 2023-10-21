8fce1119fa4430e9d2d2eb74ab6ca8a466960ce0
Sawairazia <sawairazia3@gmail.com>
2023-10-22 01:03:40 +0500

    completed

--- Program.cs
+++ Program.cs
@@ -10,7 +10,16 @@ namespace Assignment_1
         {
             File1();
             File2();
+            File3();
+            File4();
+            File5();
+            File6();
+            File7();
+            File8();
+            File9();
+            File10();
         }
+
 
         static void File1()
         {
@@ -27,7 +36,74 @@ namespace Assignment_1
             bool DrivingTestPassed = true;
             float PassingRate = 2.99999f;
         }
+
+        static void File3()
+        {
+            string CarName = " Corolla";
+            int CarSpeed = 49;
+            bool DrivingTestPassed = true;
+            float PassingRate = 1.99999f;
+        }
+
+        static void File4()
+        {
+            string CarName = " Corolla";
+            int CarSpeed = 59;
+            bool DrivingTestPassed = true;
+            float PassingRate = 3.99999f;
+
+        }
+
+        static void File5()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 60;
+            bool DrivingTestPassed = true;
+            float PassingRate = 4.99999f;
+
+        }
+
+        static void File6()
+        {
+            string CarName = " Corolla";
+            int CarSpeed = 64;
+            bool DrivingTestPassed = true;
+            float PassingRate = 5.99999f;
+
+        }
+
+        static void File7()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 68;
+            bool DrivingTestPassed = true;
+            float PassingRate = 6.99999f;
+        }
+
+        static void File8()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 70;
+            bool DrivingTestPassed = true;
+            float PassingRate = 7.99999f;
+
+        }
+
+        static void File9()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 75;
+            bool DrivingTestPassed = true;
+            float PassingRate = 8.99999f;
+
+        }
+
+        static void File10()
+        {
+            string CarName = "Corolla";
+            int CarSpeed = 80;
+            bool DrivingTestPassed = true;
+            float PassingRate = 9.9999f;
+        }
     }
 }
-
-
